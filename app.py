33142f7fb532547b8dcf89599e60149dcf47961d
Shannon Mathers <smathers@paloaltonetworks.com>
2023-09-18 13:31:09 -0500

    Updated docker-image.yml

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ alive = int(os.getenv('tokentimetolive', 60))
 
 # start the app with port 5000 and debug on!
 if __name__ == '__main__':
-    vuln_app.run(host='0.0.0.0', port=5001, debug=True)
+    vuln_app.run(host='0.0.0.0', port=5000, debug=True)
