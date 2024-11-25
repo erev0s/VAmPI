d7f1f93093e3a9c119726dbf5c3c312166c6999d
erev0s <erev0s@mail.com>
2024-11-25 14:58:24 +0200

    custom generic error

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 import connexion
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
+from connexion.exceptions import ProblemException
 
 vuln_app = connexion.App(__name__, specification_dir='./openapi_specs')
 
@@ -13,13 +14,14 @@ vuln_app.app.config['SECRET_KEY'] = 'random'
 # start the db
 db = SQLAlchemy(vuln_app.app)
 
-
-@vuln_app.app.errorhandler(401)
-def custom_401(error):
-    # Custom 401 to match the original response sent by Vampi
-    response = jsonify({"status": "fail", "message": "Invalid token. Please log in again."})
-    response.status_code = 401
+def custom_problem_handler(error):
+    # Custom error handler for clarity in structure
+    response = jsonify({
+        "status": "fail",
+        "message": getattr(error, "detail", "An error occurred"),
+    })
+    response.status_code = error.status
     return response
-
+vuln_app.add_error_handler(ProblemException, custom_problem_handler)
 
 vuln_app.add_api('openapi3.yml')
