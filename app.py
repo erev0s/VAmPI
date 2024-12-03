c1c99f788e225faa8dfeafe91a41c0c95fb56040
alexkutsan <alexkutsan@gmail.com>
2024-12-03 13:11:55 +0400

    feat: support external random seed

--- app.py
+++ app.py
@@ -14,4 +14,5 @@ alive = int(os.getenv('tokentimetolive', 60))
 
 # start the app with port 5000 and debug on!
 if __name__ == '__main__':
-    vuln_app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.getenv('PORT', 5000))
+    vuln_app.run(host='0.0.0.0', port=port, debug=True)
